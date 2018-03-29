9d12f7554ee7a2bab9b10bd7ee01d5536cb32d3f
night-crawler <lilo.panic@gmail.com>
2018-03-30 01:04:52 +0300

    fixed tests

--- src/features/actions/redisRpc.test.js
+++ src/features/actions/redisRpc.test.js
@@ -1,5 +1,4 @@
 import { RedisRpc } from '.';
-import { RSAA } from 'redux-api-middleware';
 
 
 describe('RedisRpc', () => {
@@ -16,47 +15,6 @@ describe('RedisRpc', () => {
         const rpc = new RedisRpc({ endpoint: '/test', dispatch, instanceName: 'instance' });
 
         const action = rpc.batchExecute(['method', { a: 1 }]);
-        expect(action).toBeTruthy();
-        expect(dispatch).toHaveBeenCalled();
-    });
-
-    it('can generate correct LUA _formatMatchCountScript script', () => {
-        const rpc = new RedisRpc();
-
-        const script = rpc._formatMatchCountScript('my:key:*');
-        expect(script).toBeTruthy();
-
-        const doubleQuoteEscapedScript = rpc._formatMatchCountScript('key:has:double:quote:"');
-        expect(doubleQuoteEscapedScript).toContain('\\"');
-    });
-
-    it('can fetchMatchCount', () => {
-        const dispatch = jest.fn(action => action);
-        const rpc = new RedisRpc({ endpoint: '/test', dispatch, instanceName: 'instance' });
-
-        const action = rpc.fetchMatchCount('my:key:*');
-        expect(action).toBeTruthy();
-        expect(dispatch).toHaveBeenCalled();
-    });
-
-    it('patches fetchMatchCount types with pattern and blockSize', () => {
-        const dispatch = jest.fn(action => action);
-        const rpc = new RedisRpc({ endpoint: '/test', dispatch, instanceName: 'instance' });
-
-        const action = rpc.fetchMatchCount('my:key:*');
-        expect(action[RSAA].types.map(type => type.meta.pattern)).toEqual(
-            [ 'my:key:*', 'my:key:*', 'my:key:*' ]
-        );
-        expect(action[RSAA].types.map(type => type.meta.blockSize)).toEqual(
-            [ 50000, 50000, 50000 ]
-        );
-    });
-
-    it('can fetchMatchChunk', () => {
-        const dispatch = jest.fn(action => action);
-        const rpc = new RedisRpc({ endpoint: '/test', dispatch, instanceName: 'instance' });
-
-        const action = rpc.fetchMatchChunk('my:key:*', 0);
         expect(action).toBeTruthy();
         expect(dispatch).toHaveBeenCalled();
     });
