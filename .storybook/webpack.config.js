63a340661ec9ad819c581879b641e89d1e5fb849
night-crawler <lilo.panic@gmail.com>
2018-10-21 11:08:34 +0300

    fixed storybook yaml loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.yml/,
-                loaders: [ 'yml-loader' ],
-                // include: path.resolve(__dirname, "../")
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ],
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000'
-            }
+  module: {
+    rules: [
+      {
+        test: /\.ya?ml$/,
+        use: [
+          { loader: 'js-yaml-loader', options: { safe: false } },
         ],
-    },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+    ],
+  },
 };
